67713b16dd2d13062f8b5cd451c130ad2b621c45
Kathryn <Kathryn@DESKTOP-1T04EBH>
2023-10-14 17:32:59 +0100

    Added Roland to CreditsScreen

--- src/RogueLike/Main/Screens/CreditsScreen.java
+++ src/RogueLike/Main/Screens/CreditsScreen.java
@@ -9,24 +9,30 @@ public class CreditsScreen implements Screen{
 	public void displayOutput(AsciiPanel terminal) {
 		int y = 1;
 		terminal.writeCenter("+|=|+ Credits +|=|+", y++);
-		y+=3;
+		y+=2;
 		terminal.writeCenter("Design & Development:", y++);
 		terminal.writeCenter("----------------", y++);
 		terminal.writeCenter("Kathryn U.", y++);
 		y++;
 		terminal.writeCenter("GitHub: TheWelshEngineer", y++);
-		y+=4;
+		y+=3;
 		terminal.writeCenter("Helper Method Library:", y++);
 		terminal.writeCenter("----------------", y++);
 		terminal.writeCenter("Ben T.", y++);
 		y++;
 		terminal.writeCenter("GitHub: BenTaylor25", y++);
-		y+=4;
+		y+=3;
 		terminal.writeCenter("AsciiPanel Framework & Initial Inspiration:", y++);
 		terminal.writeCenter("----------------", y++);
 		terminal.writeCenter("Trystan S.", y++);
 		y++;
 		terminal.writeCenter("GitHub: trystan", y++);
+		y+=3;
+		terminal.writeCenter("Code Review and GitHub Documentation:", y++);
+		terminal.writeCenter("----------------", y++);
+		terminal.writeCenter("H. Roland R.", y++);
+		y++;
+		terminal.writeCenter("GitHub: RolandReff", y++);
 		y++;
 		terminal.writeCenter("-- [ESCAPE]: Back --", 38);
 
