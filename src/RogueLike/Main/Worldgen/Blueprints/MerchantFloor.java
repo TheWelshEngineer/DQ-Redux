6fa74b8e94ad1c6589e3d761e3e4dbefc745fba2
diamond-deluxe <112557036+diamond-deluxe@users.noreply.github.com>
2024-07-07 19:50:17 +0100

    feat: framework for merchant floor entry stairs

--- src/RogueLike/Main/Worldgen/Blueprints/MerchantFloor.java
+++ src/RogueLike/Main/Worldgen/Blueprints/MerchantFloor.java
@@ -1,8 +1,13 @@
 package RogueLike.Main.Worldgen.Blueprints;
 
+import RogueLike.Main.AoE.Point;
 import RogueLike.Main.Tile;
 import RogueLike.Main.WorldBuilder;
 import RogueLike.Main.Worldgen.Blueprint;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MerchantFloor extends Blueprint {
 	private final int depth;
@@ -19,6 +24,15 @@ public class MerchantFloor extends Blueprint {
 		this.lowerY = (builder.height()/2)-5;
 		this.upperX = (builder.width()/2)+5;
 		this.upperY = (builder.height()/2)+5;
+	}
+
+	private boolean isAboveMerchantRoom(int x, int y, int margin) {
+		return (
+			x >= lowerX - margin
+			&& x < upperX + margin
+			&& y >= lowerY - margin
+			&& y < upperY + margin
+		);
 	}
 
 	@Override
@@ -44,21 +58,50 @@ public class MerchantFloor extends Blueprint {
 	}
 
 	@Override
-	public void onPostTileGeneration() {
-		// Force add stairs
+	public void onPostRegionConnection() {
+		// Force add stairs leading downwards.
 		builder.setTile(upperX-1, upperY-1, depth, Tile.STAIRS_DOWN);
 		builder.setTilesCircle(Tile.FLOOR, upperX-1, upperY-1, 7, depth+1);
 		builder.setTile(upperX-1, upperY-1, depth+1, Tile.STAIRS_UP);
 
-		// TODO(diamond): integrate with natural generation to add multiple stairs down into the merchant area
-		//  with teleporters that take you between the stairs and the merchant's room
-		builder.setTile(lowerX, lowerY, depth, Tile.STAIRS_UP);
-		builder.setTilesCircle(Tile.FLOOR, lowerX, lowerY, 7, depth-1);
-		builder.setTile(lowerX, lowerY, depth-1, Tile.STAIRS_DOWN);
+		// Add stairs in random locations to the floor above
+		List<Point> possibleStairsLocations = new ArrayList<>();
+		int margin = 4; // margin away from the edge
+		for (int x = margin; x < builder.width() - margin; x++) {
+			for (int y = margin; y < builder.height() - margin; y++) {
+				if (
+					builder.getTile(x, y, depth-1) == Tile.FLOOR
+					&& !isAboveMerchantRoom(x, y, margin + 2)
+				) {
+					possibleStairsLocations.add(new Point(x, y, depth-1));
+				}
+			}
+		}
+		Collections.shuffle(possibleStairsLocations);
+
+		// always place at least 4 stairs
+		int numStairs = Math.max(possibleStairsLocations.size() / 500, 4);
+		for (int i=0; i < numStairs; i++) {
+			Point point = possibleStairsLocations.get(i);
+			generateStairsUpRoom(point.x, point.y);
+		}
+	}
+
+	private void generateStairsUpRoom(int centerX, int centerY) {
+		// Carve out floor area
+		System.out.printf("Stairs up generated at (%d, %d) %n", centerX, centerY);
+		for (int dx = -1; dx <= 1; dx++) {
+			for (int dy = -1; dy <= 3; dy ++) {
+				builder.setTile(centerX + dx, centerY + dy, depth, Tile.FLOOR);
+			}
+		}
+		// Add the stairs
+		builder.setTile(centerX, centerY, depth, Tile.STAIRS_UP);
+		builder.setTile(centerX, centerY, depth-1, Tile.STAIRS_DOWN);
 	}
 
 	@Override
-	public void onPostRegionConnection() {}
+	public void onPostTileGeneration() {}
 
 
 }
