6d405cb028d5648b0c5aa337a4f22e2f28651827
diamond-deluxe <112557036+diamond-deluxe@users.noreply.github.com>
2024-07-26 13:30:34 +0100

    refactor: skill descriptions are now stored on the Skill enum itself

--- src/RogueLike/Main/Screens/ChooseSkillScreen.java
+++ src/RogueLike/Main/Screens/ChooseSkillScreen.java
@@ -6,6 +6,7 @@ import RogueLike.Main.ExtendedAsciiPanel;
 import RogueLike.Main.Managers.KeybindManager;
 import RogueLike.Main.Skill;
 import RogueLike.Main.Skillset;
+import RogueLike.Main.TextUtils;
 import RogueLike.Main.Utils.PlayerBuildDetails;
 
 import java.awt.event.KeyEvent;
@@ -130,136 +131,8 @@ public class ChooseSkillScreen implements Screen {
                 4,
                 y++);
 
-        switch (selectedSkill()) {
-            case SIMPLE_WEAPONS:
-                terminal.writeCenter(
-                        "Simple Weapons represents your skill with simple weapons", y += 5);
-                terminal.writeCenter(
-                        "such as clubs and handaxes. A higher Simple Weapons skill", y += 1);
-                terminal.writeCenter("allows you to use more powerful simple weapons,", y += 1);
-                terminal.writeCenter(
-                        "and improves your accuracy with weapons of this type.", y += 1);
-                break;
-            case MARTIAL_WEAPONS:
-                terminal.writeCenter(
-                        "Martial Weapons represents your skill with martial weapons", y += 5);
-                terminal.writeCenter(
-                        "such as longswords and battleaxes. A higher Martial Weapons skill",
-                        y += 1);
-                terminal.writeCenter("allows you to use more powerful martial weapons,", y += 1);
-                terminal.writeCenter(
-                        "and improves your accuracy with weapons of this type.", y += 1);
-                break;
-            case ARMOR_TRAINING:
-                terminal.writeCenter(
-                        "Armor Training represents your ability to move and fight", y += 5);
-                terminal.writeCenter("whilst wearing armor. A higher Armor Training skill", y += 1);
-                terminal.writeCenter(
-                        "allows you to use heavier armor and shields, and provides", y += 1);
-                terminal.writeCenter(
-                        "further bonuses to your Armor Class at higher levels.", y += 1);
-                break;
-            case FORTITUDE:
-                terminal.writeCenter(
-                        "Fortitude is a measure of your stamina and constitution.", y += 5);
-                terminal.writeCenter(
-                        "A higher Fortitude skill increases the length of time you", y += 1);
-                terminal.writeCenter(
-                        "can go without eating, and lessens the negative effects", y += 1);
-                terminal.writeCenter("incurred by eating certain food items.", y += 1);
-                break;
-            case FINESSE_WEAPONS:
-                terminal.writeCenter(
-                        "Finesse Weapons represents your skill with finesse weapons", y += 5);
-                terminal.writeCenter(
-                        "such as daggers and rapiers. A higher Finesse Weapons skill", y += 1);
-                terminal.writeCenter("allows you to use more powerful finesse weapons,", y += 1);
-                terminal.writeCenter(
-                        "and improves your accuracy with weapons of this type.", y += 1);
-                break;
-            case RANGED_WEAPONS:
-                terminal.writeCenter(
-                        "Ranged Weapons represents your skill with ranged weapons", y += 5);
-                terminal.writeCenter(
-                        "such as bows and crossbows. A higher Ranged Weapons skill", y += 1);
-                terminal.writeCenter("allows you to use more powerful ranged weapons,", y += 1);
-                terminal.writeCenter(
-                        "and improves your accuracy with weapons of this type.", y += 1);
-                break;
-            case STEALTH:
-                terminal.writeCenter("Stealth represents your ability to move unnoticed.", y += 5);
-                terminal.writeCenter(
-                        "A higher Stealth skill allows you to sneak past sleeping", y += 1);
-                terminal.writeCenter(
-                        "and unalerted foes more reliably, giving you the upper", y += 1);
-                terminal.writeCenter(
-                        "hand in combat and making it easier to avoid a fight.", y += 1);
-                break;
-            case PERCEPTION:
-                terminal.writeCenter(
-                        "Perception is a measure of your awareness and eye for detail.", y += 5);
-                terminal.writeCenter(
-                        "A higher Perception skill allows you to spot traps and other", y += 1);
-                terminal.writeCenter(
-                        "hidden things with greater ease, as well as improving", y += 1);
-                terminal.writeCenter("your chances of identifying unknown items.", y += 1);
-                break;
-            case EVOCATION:
-                terminal.writeCenter(
-                        "Evocation is a school of magic focused mainly on the", y += 5);
-                terminal.writeCenter("manipulation of raw magic. A higher Evocation skill", y += 1);
-                terminal.writeCenter("allows you to use more powerful evocation wands,", y += 1);
-                terminal.writeCenter(
-                        "and improves the effectiveness of your evocation wands.", y += 1);
-                break;
-            case PYROMANCY:
-                terminal.writeCenter(
-                        "Pyromancy is a school of magic focused mainly on the", y += 5);
-                terminal.writeCenter(
-                        "manipulation of fire and heat. A higher Pyromancy skill", y += 1);
-                terminal.writeCenter("allows you to use more powerful pyromancy wands,", y += 1);
-                terminal.writeCenter(
-                        "and improves the effectiveness of your pyromancy wands.", y += 1);
-                break;
-            case CRYOMANCY:
-                terminal.writeCenter(
-                        "Cryomancy is a school of magic focused mainly on the", y += 5);
-                terminal.writeCenter(
-                        "manipulation of water and ice. A higher Cryomancy skill", y += 1);
-                terminal.writeCenter("allows you to use more powerful cryomancy wands,", y += 1);
-                terminal.writeCenter(
-                        "and improves the effectiveness of your cryomancy wands.", y += 1);
-                break;
-            case ELECTROMANCY:
-                terminal.writeCenter(
-                        "Electromancy is a school of magic focused mainly on the", y += 5);
-                terminal.writeCenter(
-                        "manipulation of electrical energy. A higher Electromancy skill", y += 1);
-                terminal.writeCenter("allows you to use more powerful electromancy wands,", y += 1);
-                terminal.writeCenter(
-                        "and improves the effectiveness of your electromancy wands.", y += 1);
-                break;
-            case ALCHEMANCY:
-                terminal.writeCenter(
-                        "Alchemancy is a school of magic focused mainly on the", y += 5);
-                terminal.writeCenter(
-                        "manipulation of chemical processes. A higher Alchemancy skill", y += 1);
-                terminal.writeCenter("allows you to use more powerful alchemancy wands,", y += 1);
-                terminal.writeCenter(
-                        "and improves the effectiveness of your alchemancy wands.", y += 1);
-                break;
-            case FERROMANCY:
-                terminal.writeCenter(
-                        "Ferromancy is a school of magic focused mainly on the", y += 5);
-                terminal.writeCenter(
-                        "manipulation of stone and metals. A higher Ferromancy skill", y += 1);
-                terminal.writeCenter("allows you to use more powerful ferromancy wands,", y += 1);
-                terminal.writeCenter(
-                        "and improves the effectiveness of your ferromancy wands.", y += 1);
-                break;
-            default:
-                throw new IllegalArgumentException(selectedSkill().toString());
-        }
+        y += 5;
+        terminal.writeCenterMultiline(TextUtils.wordWrap(selectedSkill().description, 66, 0), y);
 
         if (skillPoints < 1) {
             terminal.writeCenter(
